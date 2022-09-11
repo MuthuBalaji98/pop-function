8ae47de86e74d30c21b869a47b4b60af5daa2c08
MuthuBalaji98 <muthubalaji1998@gmail.com>
2022-09-11 21:07:54 +0530

    pop

---
+++ main.js
@@ -0,0 +1,7 @@
+const subject=["Tamil","English","Maths","Science","Social"];
+console.log("original array :",subject);
+console.log("length of the array is "+subject.length);
+console.log("method : array.pop()");
+subject.pop();
+console.log("change in array : ",subject);
+console.log("return: length of array after pop() is "+subject.length);
